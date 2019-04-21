8c6ae43ca69546ae6fa460ccbdded1872e292f4c
Ellie <Eleanor@unknown.home>
2019-04-21 09:46:18 +0100

    JQuery to fade in checkboxes and backtotop
    Added JQuery to make the selection checkboxes fade in and to have a back to top button on page 2 and Index.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,29 @@ $(document).ready(function(){
         $("." + inputValue).toggle();
     });
 });
+
+
+$(window).scroll(scrollFunction);
+
+function scrollFunction(){
+  if ($(window).scrollTop() > 600){
+    $("#go-top-button").fadeIn();
+  } else {
+    $("#go-top-button").fadeOut();
+  }
+};
+
+function topFunction() {
+  $("html, body").animate({ scrollTop:
+  0});
+}
+
+$(window).scroll(scrollFunction2);
+
+function scrollFunction2(){
+  if ($(window).scrollTop() > 200){
+    $(".sidenav").fadeIn();
+  } else {
+    $(".sidenav").fadeOut();
+  }
+};
